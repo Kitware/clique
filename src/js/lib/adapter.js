f956f8915aff5d7c3533cf0cf8a67f37b248b907
Roni Choudhury <roni.choudhury@kitware.com>
2015-04-29 11:07:36 -0400

    Cleanup

--- src/js/lib/adapter.js
+++ src/js/lib/adapter.js
@@ -24,21 +24,16 @@
             nodeIndex[hash] = n;
         });
 
-        console.log("nodeIndex", nodeIndex);
-
         _.each(edges, function (e) {
             var sourceNode = nodes[e.source],
                 targetNode = nodes[e.target];
 
             sourceIndex[sourceNode.key] = sourceIndex[sourceNode.key] || {};
-            sourceIndex[sourceNode.key][targetNode.key] = targetNode.key
+            sourceIndex[sourceNode.key][targetNode.key] = targetNode.key;
 
             targetIndex[targetNode.key] = targetIndex[targetNode.key] || {};
             targetIndex[targetNode.key][sourceNode.key] = sourceNode.key;
         });
-
-        console.log("sourceIndex", sourceIndex);
-        console.log("targetIndex", targetIndex);
 
         return {
             getNeighborhood: function (options) {
@@ -65,14 +60,11 @@
                     frontier.add(center.key);
 
                     // Fan out from the center to reach the requested radius.
-                    _.each(_.range(options.radius), function (i) {
-                        console.log("hop", i);
-
+                    _.each(_.range(options.radius), function () {
                         var newFrontier = new cf.util.Set();
 
                         // Find all edges to and from the current frontier
                         // nodes.
-                        console.log("frontier", frontier);
                         _.each(frontier.items(), function (nodeKey) {
                             _.each(sourceIndex[nodeKey], function (neighborKey) {
                                 neighborEdges.add(JSON.stringify([nodeKey, neighborKey]));
@@ -83,13 +75,9 @@
                             });
                         });
 
-                        console.log("neighborEdges", neighborEdges.items());
-
                         // Collect the nodes named in the edges.
                         _.each(neighborEdges.items(), function (edge) {
                             edge = JSON.parse(edge);
-
-                            console.log("edge", edge);
 
                             if (!neighborNodes.has(edge[0])) {
                                 newFrontier.add(edge[0]);
@@ -103,30 +91,13 @@
                             neighborNodes.add(edge[1]);
                         });
 
-                        console.log("newFrontier", newFrontier.items());
-
                         frontier = newFrontier;
                     });
                 }
 
-                neighborNodes = neighborNodes.items();
-/*                neighborNodes = _.keys(neighborNodes).sort(function (a, b) {*/
-                    // return a - b;
-                // });
-
-                neighborEdges = neighborEdges.items();
-/*                neighborEdges = _.keys(neighborEdges).sort(function (a, b) {*/
-                    // return a[0] - b[0] === 0 ? a[1] - b[1] : a[0] - b[0];
-                // });
-
-                console.log(neighborNodes);
-                console.log(neighborEdges);
-
                 return {
-                    nodes: _.map(neighborNodes, function (i) {
-                        return nodeIndex[i];
-                    }),
-                    edges: _.map(neighborEdges, function (edge) {
+                    nodes: _.map(neighborNodes.items(), _.propertyOf(nodeIndex)),
+                    edges: _.map(neighborEdges.items(), function (edge) {
                         edge = JSON.parse(edge);
 
                         return {
