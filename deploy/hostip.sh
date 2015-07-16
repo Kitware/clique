a3985029bd8621996185d3bb5a6617a375c77bf9
Roni Choudhury <roni.choudhury@kitware.com>
2015-07-16 15:35:07 -0400

    Updating hostip to give correct config

--- deploy/hostip.sh
+++ deploy/hostip.sh
@@ -1,8 +1,12 @@
 hostip=`netstat -rn | grep '^0.0.0.0' | awk '{print $2}'`
 cat <<EOF
 {
-    "host": "${hostip}",
-    "database": "year3_graphs",
-    "collection": "mentions_monica_nino_2hop_mar12"
+    "xdataHost": "${hostip}",
+    "xdataDatabase": "year3_graphs",
+    "xdataCollection": "mentions_monica_nino_2hop_mar12",
+
+    "mongoHost": "${hostip}",
+    "mongoDatabase": "year3_graphs",
+    "mongoCollection": "twittermentions"
 }
 EOF
