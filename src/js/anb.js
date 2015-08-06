1fb0b2c8e14972253cf1ef243f56625d9f9ace17
Roni Choudhury <roni.choudhury@kitware.com>
2015-08-06 14:59:28 -0400

    Adapted anb app to work with anb data.

--- src/js/anb.js
+++ src/js/anb.js
@@ -1,107 +1,48 @@
 /*jshint browser: true, jquery: true */
 /*global clique, _, tangelo */
 
-function randomGraph(n, pct) {
-    "use strict";
-
-    var alphabet = "abcdefghijklmnopqrstuvwxyz",
-        nodes = [],
-        links = [];
-
-    _.each(_.range(n), function (i) {
-        nodes.push({
-            name: alphabet[i]
-        });
-    });
-
-    _.each(_.range(n), function (source, i) {
-        _.each(_.range(i + 1, n), function (target) {
-            if (Math.random() < pct) {
-                links.push({
-                    source: source,
-                    target: target
-                });
-            }
-        });
-    });
-
-    return {
-        nodes: nodes,
-        links: links
-    };
-}
-
 $(function () {
     "use strict";
 
     var launch = function (cfg) {
-        var graphData,
-            graph,
+        var graph,
             view,
-            mode,
             info;
 
-        mode = tangelo.queryArguments().mode || cfg.mode || "demo";
+        window.graph = graph = new clique.Graph({
+            adapter: tangelo.getPlugin("mongo").Mongo,
+            options: {
+                host: cfg.host || "localhost",
+                database: cfg.database,
+                collection: cfg.collection
+            }
+        });
 
-        if (mode === "demo") {
-            mode = "demo";
-
-            graphData = randomGraph(26, 0.20);
-
-            window.graph = graph = new clique.Graph({
-                adapter: clique.adapter.NodeLinkList,
-                options: graphData
-            });
-        } else if (mode === "xdata") {
-            mode = "xdata";
-
-            window.graph = graph = new clique.Graph({
-                adapter: tangelo.getPlugin("mongo-xdata").MongoXdata,
-                options: {
-                    host: cfg.xdataHost || "localhost",
-                    database: cfg.xdataDatabase || "year3_graphs",
-                    collection: cfg.xdataCollection || "mentions_monica_nino_2hop_mar12"
-                }
-            });
-        } else if (mode === "mongo") {
-            mode = "mongo";
-
-            window.graph = graph = new clique.Graph({
-                adapter: tangelo.getPlugin("mongo").Mongo,
-                options: {
-                    host: cfg.mongoHost || "localhost",
-                    database: cfg.mongoDatabase || "year3_graphs",
-                    collection: cfg.mongoCollection || "twittermentions"
-                }
-            });
-        }
-
-        $("#seed").on("click", function () {
-            var name = $("#name").val().trim(),
-                spec = {},
-                radiusText = $("#radius").val().trim(),
-                radius = Number(radiusText),
+        $("#submit").on("click", function () {
+            var label = $("#label").val().trim(),
+                filename = $("#filename").val().trim(),
+                spec = {};
                 delsearch = $("#delsearch").prop("checked");
 
-            if (name === "" || radiusText === "" || isNaN(radius)) {
+            if (label === "" && filename === "") {
                 return;
             }
 
-            spec[mode === "xdata" ? "username" : "name"] = name;
+            if (filename) {
+                spec.filename = filename;
+            }
+
+            if (label) {
+                spec.label = label;
+            }
+
             graph.adapter.findNode(spec)
                 .then(function (center) {
+                    console.log(center);
                     if (center) {
-                        graph.addNeighborhood({
-                            center: center,
-                            radius: radius,
-                            deleted: delsearch
-                        });
+                        graph.addNode(center);
                     }
                 });
-        });
-
-        $("#save").on("click", function () {
-            graph.adapter.sync();
         });
 
         window.view = view = new clique.view.Cola({
@@ -117,5 +58,5 @@ $(function () {
         info.render();
     };
 
-    $.getJSON("clique.json").then(launch, _.bind(launch, {}));
+    $.getJSON("anb.json").then(launch, _.bind(launch, {}));
 });
