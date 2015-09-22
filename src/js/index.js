9181aed302d6a090d505278efc611c8d5c668960
Roni Choudhury <roni.choudhury@kitware.com>
2015-09-22 13:37:32 -0400

    Removing "mode" stuff to simplify application

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 /*jshint browser: true, jquery: true */
-/*global app, clique, _, tangelo */
+/*global app, clique, _ */
 
 function randomGraph(n, pct) {
     "use strict";
@@ -34,92 +34,58 @@ function randomGraph(n, pct) {
 $(function () {
     "use strict";
 
-    var launch = function (cfg) {
-        var graphData,
-            graph,
-            view,
-            mode,
-            info;
+    var graphData,
+        graph,
+        view,
+        info;
 
-        mode = tangelo.queryArguments().mode || cfg.mode || "demo";
+    graphData = randomGraph(26, 0.20);
+    graph = new clique.Graph({
+        adapter: clique.adapter.NodeLinkList,
+        options: graphData
+    });
 
-        if (mode === "demo") {
-            mode = "demo";
+    $("#seed").on("click", function () {
+        var name = $("#name").val().trim(),
+            spec,
+            radiusText = $("#radius").val().trim(),
+            radius = Number(radiusText),
+            delsearch = $("#delsearch").prop("checked");
 
-            window.graphData = graphData = randomGraph(26, 0.20);
-
-            window.graph = graph = new clique.Graph({
-                adapter: clique.adapter.NodeLinkList,
-                options: graphData
-            });
-        } else if (mode === "xdata") {
-            mode = "xdata";
-
-            window.graph = graph = new clique.Graph({
-                adapter: tangelo.getPlugin("mongo-xdata").MongoXdata,
-                options: {
-                    host: cfg.xdataHost || "localhost",
-                    database: cfg.xdataDatabase || "year3_graphs",
-                    collection: cfg.xdataCollection || "mentions_monica_nino_2hop_mar12"
-                }
-            });
-        } else if (mode === "mongo") {
-            mode = "mongo";
-
-            window.graph = graph = new clique.Graph({
-                adapter: tangelo.getPlugin("mongo").Mongo,
-                options: {
-                    host: cfg.mongoHost || "localhost",
-                    database: cfg.mongoDatabase || "year3_graphs",
-                    collection: cfg.mongoCollection || "twittermentions"
-                }
-            });
+        if (name === "" || radiusText === "" || isNaN(radius)) {
+            return;
         }
 
-        $("#seed").on("click", function () {
-            var name = $("#name").val().trim(),
-                spec,
-                radiusText = $("#radius").val().trim(),
-                radius = Number(radiusText),
-                delsearch = $("#delsearch").prop("checked");
+        spec = {
+            queryOp: "==",
+            field: "name",
+            value: name
+        };
+        graph.adapter.findNode(spec)
+            .then(function (center) {
+                if (center) {
+                    graph.addNeighborhood({
+                        center: center,
+                        radius: radius,
+                        deleted: delsearch
+                    });
+                }
+            });
+    });
 
-            if (name === "" || radiusText === "" || isNaN(radius)) {
-                return;
-            }
+    $("#save").on("click", function () {
+        graph.adapter.sync();
+    });
 
-            spec = {
-                queryOp: "==",
-                field: mode === "xdata" ? "username" : "name",
-                value: name
-            };
-            graph.adapter.findNode(spec)
-                .then(function (center) {
-                    if (center) {
-                        graph.addNeighborhood({
-                            center: center,
-                            radius: radius,
-                            deleted: delsearch
-                        });
-                    }
-                });
-        });
+    view = new clique.view.Cola({
+        model: graph,
+        el: "#content"
+    });
 
-        $("#save").on("click", function () {
-            graph.adapter.sync();
-        });
-
-        window.view = view = new clique.view.Cola({
-            model: graph,
-            el: "#content"
-        });
-
-        window.info = info = new app.view.SelectionInfo({
-            model: view.selection,
-            el: "#info",
-            graph: graph
-        });
-        info.render();
-    };
-
-    $.getJSON("clique.json").then(launch, _.bind(launch, {}));
+    info = new app.view.SelectionInfo({
+        model: view.selection,
+        el: "#info",
+        graph: graph
+    });
+    info.render();
 });
