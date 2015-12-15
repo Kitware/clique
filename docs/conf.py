2ffb507c564bd7365ec668af318ae02062218aae
Roni Choudhury <roni.choudhury@kitware.com>
2015-12-15 16:46:08 -0500

    Modify config to use sphinx_rtd theme if available

--- docs/conf.py
+++ docs/conf.py
@@ -103,7 +103,19 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+rtd_present = False
+use_rtd = True
+try:
+    import sphinx_rtd_theme
+    rtd_present = True
+except ImportError:
+    pass
+
+if rtd_present and use_rtd:
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+else:
+    html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
