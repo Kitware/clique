05add27997d2da2fe080699c332e264c9df8d747
Roni Choudhury <roni.choudhury@kitware.com>
2016-02-25 07:44:49 -0500

    Update clique structure test

--- test/clique-exports.js
+++ test/clique-exports.js
@@ -1,6 +1,6 @@
 import test from 'tape';
 
-import clique from './../src/clique.js';
+import clique from './../src/clique';
 
 test('Check for all parts of ES5-compatible clique object', t => {
   t.ok(clique, 'clique exists');
